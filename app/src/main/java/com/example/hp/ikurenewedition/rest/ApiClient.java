c2cd7d6c3be636eaa3049a991b541714619aba9a
agnibrainhack <32513170+agnibrainhack@users.noreply.github.com>
2018-03-13 21:46:33 +0530

    Update ApiClient.java

--- app/src/main/java/com/example/hp/ikurenewedition/rest/ApiClient.java
+++ app/src/main/java/com/example/hp/ikurenewedition/rest/ApiClient.java
@@ -8,7 +8,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
  */
 
 public class ApiClient {
-    public static final String BASE_URL = "http://ikurelive.com/app/prescription/api/";
+    public static final String BASE_URL = "urlhere";
     private static Retrofit retrofit = null;
 
     public static Retrofit getClient(){
